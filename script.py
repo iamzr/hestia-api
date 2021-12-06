7d3e77d6d9fe73e4c43d50a3495af35c8845faf3
iamzr <iamziarehman@gmail.com>
2021-12-06 18:30:53 +0000

    Using postman for testing

--- script.py
+++ script.py
@@ -1,4 +1,18 @@
 import sys
 from hestia_earth.utils.table import pivot_csv
+import csv
+from io import StringIO
 
-print(pivot_csv(sys.argv[1]).to_string())
+with open('./tests/sample.csv', 'r') as file:
+    table = pivot_csv(file)
+
+    # creating empty stream
+    buffer = StringIO()
+
+    # adding data to stream
+    table.to_csv(buffer, index =False)
+
+    # printing stream
+    print(buffer.getvalue())
+
+
