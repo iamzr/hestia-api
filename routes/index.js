e60e114c27ad069590acbb4c9f2ecf4e04875727
iamzr <iamziarehman@gmail.com>
2021-12-06 23:13:01 +0000

    Added swagger documentation

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,32 @@ const router = express.Router();
 const csvController = require("../controllers/csvUploadController");
 const upload = require("../middlewares/upload.js");
 
+/**
+ * @swagger
+ *  /:
+ *    post:
+ *      summary: Use to upload csv file
+ *      consumes:
+ *        - multipart/form-data
+ *      produces:
+ *        - text/html
+ *      parameters:
+ *        - in: formData
+ *          name: file
+ *          type: file
+ *          description: the csv file to be uploaded
+ *      responses:
+ *        "200":
+ *          description: A successful response
+ *        "400":
+ *          description: Bad request
+ *        "415":
+ *          description: Uploaded a file that wasn't a CSV file
+ *        "500":
+ *          description: Internal error. Unable to process the CSV file
+ *
+ *
+ */
 router.post("/", csvController.upload, csvController.runPyScript);
 
 router.get("/", (req, res, next) => {
