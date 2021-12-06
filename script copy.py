78bf0806e71b0588d45da90d918aefa90f77e3a6
iamzr <iamziarehman@gmail.com>
2021-12-06 20:41:16 +0000

    Edited script.py output as csv

--- script copy.py
+++
@@ -1,7 +0,0 @@
-import sys
-from hestia_earth.utils.table import pivot_csv
-import csv
-
-with open('./tests/sample.csv', 'r') as file:
-    reader = csv.reader(file)
-    print(pivot_csv(file))
--- script.py
+++ script.py
@@ -3,16 +3,14 @@ from hestia_earth.utils.table import pivot_csv
 import csv
 from io import StringIO
 
-with open('./tests/sample.csv', 'r') as file:
-    table = pivot_csv(file)
+table = pivot_csv(sys.argv[1])
 
-    # creating empty stream
-    buffer = StringIO()
+# creating empty stream
+buffer = StringIO()
 
-    # adding data to stream
-    table.to_csv(buffer, index =False)
+# adding data to stream
+table.to_csv(buffer, index =False)
 
-    # printing stream
-    print(buffer.getvalue())
-
+# printing stream
+print(buffer.getvalue())
 
