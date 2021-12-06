d135f5f3911eb1fd421a8fcd9f607ebae4e12ae9
iamzr <iamziarehman@gmail.com>
2021-12-06 23:46:39 +0000

    Clean up

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -14,16 +14,6 @@ const csvFilter = (req, file, cb) => {
   }
 };
 
-// var storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "/tmp/csv");
-//   },
-//   filename: (req, file, cb) => {
-//     console.log(file.originalname);
-//     cb(null, `${Date.now()}-${file.originalname}`);
-//   },
-// });
-
 var uploadFile = multer({ dest: "/tmp/csv", fileFilter: csvFilter });
 
 module.exports = uploadFile;
