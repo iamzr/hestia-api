d7488caf8162f0c4d4ee47e413e452e8e32cb000
iamzr <iamziarehman@gmail.com>
2021-12-06 21:10:40 +0000

    Cleaned up index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const http = require("http");
-const fs = require("fs");
-
 const express = require("express");
-const spawn = require("child_process").spawn;
-const multer = require("express");
-const csv = require("fast-csv");
-
-const upload = multer({ dest: "tmp/csv" });
 
 const indexRouter = require("./routes/index");
-
-global.__basedir = __dirname + "/..";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
